364747239260ec298cc65777f3f1e92c5e42ecc9
bruno <bruno@192.168.35.115>
2022-06-08 09:33:38 -0300

    Changes

--- teste1/src/teste1/Hello.java
+++ teste1/src/teste1/Hello.java
@@ -2,7 +2,7 @@ package teste1;
 
 public class Hello {
 	public static void main(String[] args) {
+		// 80 caracteres é o número ideal por linha
 		System.out.println("Hello World");
-		System.out.println("80 caracteres é o número ideal por linha");
 	}
 }
