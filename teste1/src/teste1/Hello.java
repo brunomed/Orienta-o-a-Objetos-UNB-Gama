d532e016a8d04235ff9e3c67be51a4c5550305aa
bruno <bruno@192.168.35.115>
2022-06-08 09:25:11 -0300

    Adicionei uma observação

--- teste1/src/teste1/Hello.java
+++ teste1/src/teste1/Hello.java
@@ -3,6 +3,6 @@ package teste1;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Teste");
+		System.out.println("80 caracteres é o máximo por linha para o ideal");
 	}
 }
